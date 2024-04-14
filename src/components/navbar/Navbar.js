80354a430a8a189a3aea942fdfac62661400a4ab
novanmartejares <novantejares@gmail.com>
2024-04-14 15:23:40 +0800

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -110,7 +110,7 @@ function Navbarcomponent({ home = false }) {
             <div className={styles.navleft}>
                 <div className={styles.logoContainer}>
                     <Link href="/" className={styles.logoLink}>
-                        Airin
+                        PapaWatch
                     </Link>
                 </div>
                 <div className={styles.navItemsContainer}>
